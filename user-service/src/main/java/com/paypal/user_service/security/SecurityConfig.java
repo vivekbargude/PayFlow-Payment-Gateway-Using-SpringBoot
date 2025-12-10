51e495dafd9befb85ecd3a4ed1e359c6bbf44e93
vivekbargude <vivekbargude1206@gmail.com>
2025-12-10 12:40:20 +0530

    user-service created

---
+++ user-service/src/main/java/com/paypal/user_service/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.paypal.user_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+}
