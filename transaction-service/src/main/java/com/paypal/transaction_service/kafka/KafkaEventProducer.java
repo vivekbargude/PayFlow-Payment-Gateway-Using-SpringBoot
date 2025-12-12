26a0e9b3cde97bb72c7922756ec23a73a01677ce
vivekbargude <vivekbargude1206@gmail.com>
2025-12-12 14:24:24 +0530

    added docker-compose file to create kafka and zookeeper container and integrated kafka with transaction-service

--- transaction-service/src/main/java/com/paypal/transaction_service/kafka/KafkaEventProducer.java
+++ transaction-service/src/main/java/com/paypal/transaction_service/kafka/KafkaEventProducer.java
@@ -1,4 +1,43 @@
 package com.paypal.transaction_service.kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.paypal.transaction_service.entity.Transaction;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
 public class KafkaEventProducer {
+
+    private static final String TOPIC = "txn-initiated";
+
+    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public KafkaEventProducer(KafkaTemplate<String, Transaction> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+        // Register module to handle Java 8 date/time serialization
+        this.objectMapper.registerModule(new JavaTimeModule());
+    }
+    public void sendTransactionEvent(String key, Transaction transaction) {
+        System.out.println("📤 Sending to Kafka → Topic: " + TOPIC + ", Key: " + key + ", Message: " + transaction);
+
+        CompletableFuture<SendResult<String, Transaction>> future = kafkaTemplate.send(TOPIC, key, transaction);
+
+        future.thenAccept(result -> {
+            RecordMetadata metadata = result.getRecordMetadata();
+            System.out.println("✅ Kafka message sent successfully! Topic: " + metadata.topic() + ", Partition: " + metadata.partition() + ", Offset: " + metadata.offset());
+        }).exceptionally(ex -> {
+            System.err.println("❌ Failed to send Kafka message: " + ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        });
+    }
 }
